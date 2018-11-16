6ce3aa86a709850439f04a583c4af36ae9d6ce40
Jaime Jacobo <jaimejacobordv@gmail.com>
2018-11-16 16:58:52 -0500

    x

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ class Game {
             {name: '3', value: 3}, 
             {name: '4', value: 4}, 
             {name: '5', value: 5}, 
-            {name: '6', value: 6}, 
+            {name: '6', value: 6},
             {name: '7', value: 7}, 
             {name: '8', value: 8}, 
             {name: '9', value: 9}, 
@@ -61,9 +61,9 @@ class Game {
     };
 
 
-    bet(betInt) {
+    // bet(betInt) {
 
-    };
+    // };
 
 
     shuffleAndDealCards() {
